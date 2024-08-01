86f79b5715987e479b9744502f98158c20d9585c
George Ikigu <ikigu.geo@gmail.com>
2024-08-01 16:30:59 +0300

    solution: task 0

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,23 @@
+def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be opened.
+
+    Args:
+        boxes (list): A list of lists, where each inner list represents the keys in a box.
+
+    Returns:
+        True if all boxes can be opened, False otherwise.
+    """
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+    queue = [0]
+
+    while queue:
+        box = queue.pop(0)
+        for key in boxes[box]:
+            if not visited[key]:
+                visited[key] = True
+                queue.append(key)
+
+    return all(visited)
